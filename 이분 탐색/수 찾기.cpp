2e12b473796966ba03d0bb6aa49c7dd8ceba8973
HYH <babypooh2001@naver.com>
2021-05-16 15:34:33 +0900

    수 찾기

--- 이분 탐색/수 찾기.cpp
+++ 이분 탐색/수 찾기.cpp
@@ -1,75 +1,71 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 const int MAX = 100000;
 int N, M;
 vector<int> A;
 
-int binarySearch(int low, int high, int target)
-{
-    //기저 사례: low가 high보다 크면 없는 수
-    if (low > high)
-    {
-        return 0;
-    }      
-
-    else
-    {
-        int mid = (low + high) / 2;
-
-        //해당 지점에 target이 있다면 찾았으므로 1 반환
-        if (A[mid] == target)
-        {
-            return 1;
-        }           
-
-        //해당 지점에 있는 수가 target보다 크면 왼쪽 반 탐색
-        else if (A[mid] > target)
-        {
-            return binarySearch(low, mid - 1, target);
-        }            
-
-        //해당 지점에 있는 수가 target보다 작으면 오른쪽 반 탐색
-        else
-        {
-            return binarySearch(mid + 1, high, target);
-        }           
-    }
-}
-
-
 int main()
 {
-    ios_base::sync_with_stdio(0);
+	ios_base::sync_with_stdio(0);
 
-    cin.tie(0); //cin 속도 향상 위해
-    cout.tie(0);
+	cin.tie(0); //cin 속도 향상 위해
+	cout.tie(0);
 
-    cin >> N;
+	cin >> N;
 
-    for (int i = 0; i < N; i++)
-    {
-        int num;
+	for (int i = 0; i < N; i++)
+	{
+		int nTemp = 0;
 
-        cin >> num;
+		cin >> nTemp;
 
-        A.push_back(num);
-    }
+		A.push_back(nTemp);
+	}
 
-    sort(A.begin(), A.end()); //이분탐색은 정렬되어 있어야한다.
+	sort(A.begin(), A.end());
 
-    cin >> M;
+	cin >> M;
 
-    for (int i = 0; i < M; i++)
-    {
-        int num;
+	for (int i = 0; i < M; i++)
+	{
+		int nTemp = 0;
 
-        cin >> num;
+		cin >> nTemp;
 
-        cout << binarySearch(0, N - 1, num) << "\n"; //endl 쓰면 시간 초과
-    }
+		int start = 0;
+		int end = N - 1;
+		int answer = 0;
 
-    return 0;
+		while (start <= end)
+		{
+			if (nTemp > A[N - 1])
+			{
+				break;
+			}
+
+			int mid = (start + end) / 2;
+
+			if (nTemp == A[mid])
+			{
+				answer = 1;				
+				break;
+			}
+
+			else if(nTemp < A[mid])
+			{
+				end = mid - 1;
+			}
+
+			else if (nTemp > A[mid])
+			{
+				start = mid + 1;
+			}
+		}
+
+		cout << answer << '\n';
+	}
 }
