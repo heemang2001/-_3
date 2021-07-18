89c6f19ca4eefdfef4ec036e98a7bb59f349af8b
heemang2001 <babypooh2001@naver.com>
2021-07-18 15:21:18 +0900

    숫자 구슬

--- 이분 탐색/숫자 구슬.cpp
+++ 이분 탐색/숫자 구슬.cpp
@@ -5,14 +5,23 @@ using namespace std;
 int n, m;
 int marble[301];
 
-bool maxMarbleSum(int val) {
+bool maxMarbleSum(int val)
+{
 	int cnt = 1, sum = 0;
-	for (int i = 0; i < n; ++i) {
-		if (marble[i] > val)return false;
-		if (sum + marble[i] <= val) {
+	for (int i = 0; i < n; ++i)
+	{
+		if (marble[i] > val)
+		{
+			return false;
+		}
+
+		if (sum + marble[i] <= val)
+		{
 			sum += marble[i];
 		}
-		else {
+
+		else
+		{
 			++cnt;
 			sum = marble[i];
 		}
@@ -24,48 +33,61 @@ bool maxMarbleSum(int val) {
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+
 	cin >> n >> m;
+
 	for (int i = 0; i < n; ++i)
+	{
 		cin >> marble[i];
+	}
+		
 	int l = 1, r = n * 100;
-	while (l <= r) {
+
+	while (l <= r)
+	{
 		int mid = (l + r) / 2;
-		if (maxMarbleSum(mid)) {
+
+		if (maxMarbleSum(mid))
+		{
 			r = mid - 1;
 		}
-		else {
+
+		else
+		{
 			l = mid + 1;
 		}
 	}
 
 	cout << l << endl;
-	int cnt = 0, sum = 0, group = 1;
-	vector<int> ans;
-	for (int i = 0; i < n; ++i) {
-		sum += marble[i];
-		if (sum > l) {
-			ans.push_back(cnt);
-			sum = marble[i];
-			cnt = 0;
-			++group;
-		}
-		++cnt;
-	}
-	ans.push_back(cnt);
-	int pos = ans.size() - 1;
-	while (group < m) {
-		if (ans[pos] == 1) {
-			--pos;
-		}
-		else {
-			--ans[pos];
-			ans.push_back(1);
-			++group;
-		}
-	}
-	for (int val : ans) {
-		//cout << val << " ";
-	}
+
+	//int cnt = 0, sum = 0, group = 1;
+	//vector<int> ans;
+	//for (int i = 0; i < n; ++i) {
+	//	sum += marble[i];
+	//	if (sum > l) {
+	//		ans.push_back(cnt);
+	//		sum = marble[i];
+	//		cnt = 0;
+	//		++group;
+	//	}
+	//	++cnt;
+	//}
+	//ans.push_back(cnt);
+	//int pos = ans.size() - 1;
+	//while (group < m) {
+	//	if (ans[pos] == 1) {
+	//		--pos;
+	//	}
+	//	else {
+	//		--ans[pos];
+	//		ans.push_back(1);
+	//		++group;
+	//	}
+	//}
+	//for (int val : ans) {
+	//	cout << val << " ";
+	//}
+
 	return 0;
 }
 
